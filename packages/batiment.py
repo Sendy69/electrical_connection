adf6e434c740a2de97ee7a16621d4fef1e1e1ccb
Sandrine Nokam <sandrinewato27@gmail.com>
2024-03-04 21:48:58 +0100

    made some little changes about the class Batiment

--- packages/batiment.py
+++ packages/batiment.py
@@ -3,18 +3,15 @@ from packages.infra import Infra
 #Création de la classe Batiment
 class Batiment:
     def __init__(self, id_building, list_infras) -> None:
-        self.id_building = id_building
-        self.building_diff = 0
-        
+        self.id_building = id_building     
         self.list_infras = list_infras
         
 
     #Cette méthode permet de calculer la difficulté d'un batiment en sommant les difficultés de ces infra
     def get_building_difficulty(self) -> float:
-        self.building_diff = sum(self.list_infras)
-        return self.building_diff
+        return sum(self.list_infras)
     
     # Cette méthode permet de comparer la difficulté entre 02 batiments
     def __lt__(self, other_object):
-        return self.building_diff < other_object.building_diff 
+        return self.get_building_difficulty() < other_object.get_building_difficulty() 
     
